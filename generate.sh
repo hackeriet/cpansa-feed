cd3b1c031d70fecf1d45de8019465e19fff4407f
Breno G. de Oliveira <garu@cpan.org>
2023-04-28 16:33:48 +0200

    expose public data on the website

--- generate.sh
+++ generate.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 set -ex
 mkdir -p _site
+cp -p schema.json _site/schema.json
+
 perl -V > _site/index.html
 date >> _site/index.html
+
+perl generate-cpansa-data.pl > _site/cpansa.json
