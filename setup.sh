0a6039963075439a9614667befc9e22b8a58ef24
Stig Palmquist <git@stig.io>
2023-04-29 11:45:51 +0200

    Build from latest cpan-audit and cpan-s-a submodule

--- setup.sh
+++ setup.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env sh
 set -ex
-cpm install -g
 
+
+## Normally, this is fine
+##
+# cpm install -g
+
+## But we want the latest cpan-audit and update cpan-s-a repo so we're not
+## depending on it being released on CPAN
+##
+cpanm -n --installdeps CPAN::Audit
+cpanm -n YAML::Tiny Mojolicious
+mkdir -p /app
+cd /app
+git clone https://github.com/briandfoy/cpan-audit
+cd cpan-audit
+# Get latest submodules, i.e. cpan-security-advisory
+git submodule init
+git submodule update --recursive --remote
+
+util/generate
+
+perl Makefile.PL
+make && make install
